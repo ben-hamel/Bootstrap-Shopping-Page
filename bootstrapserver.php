928d9b888f765cae19e099e258f693c1d27bd76a
Ben <benehamel@gmail.com>
2021-04-05 11:44:17 -0230

    Initial commit

---
+++ bootstrapserver.php
@@ -0,0 +1,10 @@
+<?php
+    //retrieve all 11 values from the server
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+        
+    echo "Data sent to server";
+
+
+?>
